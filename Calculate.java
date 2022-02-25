a4322a7c21514ac356a1d47203dec241bb92bfb8
lmzacher123 <96551405+lmzacher123@users.noreply.github.com>
2022-02-25 11:27:27 -0600

    Add files via upload

---
+++ Calculate.java
@@ -0,0 +1,23 @@
+package org.paca;
+
+public class Calculate {
+
+	
+	
+	int add()
+{
+	int a=15;
+	int b=20;
+	int c=a+b;
+	int d=a*b;
+	System.out.println("product of two numbers "+d);
+	return c;
+	
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calculate obj=new Calculate();
+		obj.add();
+		System.out.println("sum of two numbers "+c);
+	}
+}
