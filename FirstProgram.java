d4fbc34166e7c0857ec92f48c31245226cf8396d
lmzacher123 <96551405+lmzacher123@users.noreply.github.com>
2022-02-25 11:45:30 -0600

    Add files via upload

---
+++ FirstProgram.java
@@ -0,0 +1,11 @@
+package javapackage1;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+System.out.println("hello world");
+System.out.println("hello world");
+	}
+}
